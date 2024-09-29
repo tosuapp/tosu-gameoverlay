e43f0a2892e2e4fd6874f94eb5797514b5458167
Cherry <aazetthr@gmail.com>
2024-09-30 00:35:23 +0300

    chore: export compile commands

--- tosu_overlay/tosu_overlay_win.cc
+++ tosu_overlay/tosu_overlay_win.cc
@@ -1,29 +1,20 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
-
 #include <windows.h>
 
-#include "include/cef_command_line.h"
-#include "include/cef_sandbox_win.h"
-#include "tosu_overlay/simple_app.h"
+#include <include/cef_command_line.h>
+#include <include/cef_sandbox_win.h>
+#include <tosu_overlay/simple_app.h>
 
-// When generating projects with CMake the CEF_USE_SANDBOX value will be defined
-// automatically if using the required compiler version. Pass -DUSE_SANDBOX=OFF
-// to the CMake command-line to disable use of the sandbox.
 // Uncomment this line to manually enable sandbox support.
 // #define CEF_USE_SANDBOX 1
 
 #if defined(CEF_USE_SANDBOX)
-// The cef_sandbox.lib static library may not link successfully with all VS
-// versions.
 #pragma comment(lib, "cef_sandbox.lib")
 #endif
 
 // Entry point function for all processes.
 int APIENTRY wWinMain(HINSTANCE hInstance,
                       HINSTANCE hPrevInstance,
-                      LPTSTR lpCmdLine,
+                      LPWSTR lpCmdLine,
                       int nCmdShow) {
   UNREFERENCED_PARAMETER(hPrevInstance);
   UNREFERENCED_PARAMETER(lpCmdLine);
