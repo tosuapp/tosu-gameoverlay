b4c71fd52a383846cf7e68efb299e664790ae6f3
Mikhail Babynichev <i@kotrik.ru>
2024-11-10 22:36:36 +0300

    ci: update ci

--- build.sh
+++
@@ -1,7 +0,0 @@
-# first we compile exe
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DDESKTOP=1 -B build
-ninja -C build
-
-# then we compile dll
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DDESKTOP=0 -B build
-ninja -C build
